8c04c7afe7d76dacc966a6dc5807d00b0f748306
AdnerC <adnerc@Nycstudents.net>
2025-01-03 10:27:21 -0500

    TH-1:Added a Treasure class that stores the current treasure, and has boolean variables to remember whether they were collected. Also implementing a generate treasure method to configure which treasure we get

---
+++ src/Treasure.java
@@ -0,0 +1,20 @@
+public class Treasure {
+    private boolean treasure1;
+    private boolean treasure2;
+    private boolean treasure3;
+    private double treasureChance;
+    private int counter;
+    private String currentTreasure;
+    public Treasure(){
+        treasure1 =false;
+        treasure1 =false;
+        treasure1 =false;
+        treasureChance = Math.random();
+        counter = 0;
+    }
+
+    public void generateTreasure(){
+
+    }
+
+}
