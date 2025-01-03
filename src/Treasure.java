db1f0a2e6215504764491a2bdd81e475584ce9af
AdnerC <adnerc@Nycstudents.net>
2025-01-03 10:41:11 -0500

    TH-1: Completed generateTreasure method to create the treasure, and added a searchTreasure method to determine if they got it or not.

--- src/Treasure.java
+++ src/Treasure.java
@@ -2,19 +2,41 @@ public class Treasure {
     private boolean treasure1;
     private boolean treasure2;
     private boolean treasure3;
-    private double treasureChance;
+    private int treasureChance;
     private int counter;
     private String currentTreasure;
+
     public Treasure(){
         treasure1 =false;
         treasure1 =false;
         treasure1 =false;
-        treasureChance = Math.random();
+        treasureChance =((int) Math.random()*100)+1;
         counter = 0;
     }
 
     public void generateTreasure(){
+        double treasureSeed = Math.random();
 
+        if (treasureSeed<.33){
+                currentTreasure = "Sapphire treasure";
+            }
+        if ((treasureSeed>.33) && treasureSeed<.66){
+                currentTreasure = "Jade treasure";
+            }
+        if (treasureSeed>.66){
+                currentTreasure = "Diamond treasure";
+            }
+
+        }
+
+    public boolean searchTreasure(){
+        if (treasureChance>50){
+            return true;
+        }else {
+            return false;
+        }
     }
+
+
 
 }
